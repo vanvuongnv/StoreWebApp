c6416e3af96cf713fd0542220045d838bbf18411
vuong.nguyenvan1 <vuongnguyenv1@nashtechglobal.com>
2022-10-08 14:51:15 +0700

    api categories

---
+++ Store.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Store.Api.Data;
+
+namespace Store.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        // injection db context
+        private readonly StoreContext _context;
+        public CategoriesController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        // APIs Areas
+        [HttpGet]
+        [Route("get-categories")]
+        public IActionResult GetCategories()
+        {
+            // SELECT * FROM Categories
+            var categories = _context.Categories.ToList();
+
+            // return HTTP STATUS 200 with list categories
+            return Ok(categories);
+        }
+
+        [HttpGet]
+        [Route("get-category-by-id")]
+        public IActionResult GetCategoryById(int id)
+        {
+            // SELECT * FROM Categories WHERE CategoryId = @id
+            var category = _context.Categories
+                .Where(x => x.CategoryId == id)
+                .FirstOrDefault();
+
+            if(category is null)
+            {
+                // HTTP Status 404
+                return NotFound(new { msg = "KO_TIM_THAY" });
+            }
+            return Ok(category);
+        }
+
+        [HttpGet]
+        [Route("get-by-id/{id}")]
+        public IActionResult GetById(int id)
+        {
+            // SELECT * FROM Categories WHERE CategoryId = @id
+            var category = _context.Categories
+                .Where(x => x.CategoryId == id)
+                .FirstOrDefault();
+
+            if (category is null)
+            {
+                // HTTP Status 404
+                return NotFound(new { msg = "KO_TIM_THAY" });
+            }
+            return Ok(category);
+        }
+
+    }
+}
--- Store.Api/Program.cs
+++ Store.Api/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Store.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string connectionString = builder.Configuration.GetConnectionString("Default");
+// setup db context
+builder.Services.AddDbContext<StoreContext>(options =>
+{    
+    options.UseSqlServer(connectionString);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
