77411fb8c39a8b4c494d6ad058a73bd2922837f3
vuong.nguyenvan1 <vuongnguyenv1@nashtechglobal.com>
2022-10-29 13:38:33 +0700

    add product dto model

---
+++ Store.Api/Models/ProductDtoModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Store.Api.Models
+{
+    public class ProductDtoModel
+    {
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 3)]
+        public string? ProductName { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public double Price { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int UnitsInStock { get; set; }
+    }
+}
